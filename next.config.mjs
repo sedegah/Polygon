1698e6068569ad305b950fa80074bbb5e494196b
Kimathi  Sedegah <sedkim370@gmail.com>
2026-03-09 23:58:19 +0000

    clean up next.config.mjs and format tsconfig.json for consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['placeholder.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
     unoptimized: true,
   },
   reactStrictMode: true,
